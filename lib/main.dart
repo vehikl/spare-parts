9432d86246ea362bd6405b07a1f4cd1f9d3ce854
Vehikl Team <ques@vehikl.com>
2022-06-07 15:54:41 -0400

    Adds login email verification rule

--- lib/main.dart
+++ lib/main.dart
@@ -42,6 +42,7 @@ class MyApp extends StatelessWidget {
           if (user?.email != null && user!.email!.endsWith('vehikl.com')) {
             return const HomePage();
           }
+          
           return const SignInPage();
         },
       ),
