e246d5b5a31c4f8ca07787b6b07cdcb52f57efe1
Vehikl Team <ques@vehikl.com>
2022-09-15 16:59:33 -0400

    Co-authored-by: Andrii Denysenko <a.denysenko@vehikl.com>

---
+++ functions/src/index.ts
@@ -0,0 +1,15 @@
+import * as functions from "firebase-functions";
+
+import * as admin from 'firebase-admin'
+
+admin.initializeApp()
+
+export const getUsers = functions.https.onCall(async (data, context) => {
+  const userListResult = await admin.auth().listUsers();
+  const users = userListResult.users;
+
+  return users.map(user => ({
+    id: user.uid,
+    name: user.displayName,
+  }));
+});
