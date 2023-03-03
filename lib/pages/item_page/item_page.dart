ed14dfa86e0be82051ef1429f00764f490043f94
adenysenko <a.denysenko@vehikl.com>
2023-03-03 16:00:53 -0500

    WIP: add label printing functionality

--- lib/pages/item_page/item_page.dart
+++ lib/pages/item_page/item_page.dart
@@ -28,6 +28,35 @@ class _ItemPageState extends State<ItemPage> {
     init();
   }
 
+  void printLabel() {
+    final label = openLabelXml('''<?xml version="1.0" encoding="utf-8"?>
+<DieCutLabel Version="8.0" Units="twips">
+    <PaperOrientation>Landscape</PaperOrientation>
+    <Id>NameBadgeTag</Id>
+    <PaperName>30252 Address</PaperName>
+    <ObjectInfo>
+        <ImageObject>
+            <Name>photo</Name>
+            <ForeColor Alpha="255" Red="0" Green="0" Blue="0" />
+            <BackColor Alpha="0" Red="0" Green="0" Blue="0" />
+            <LinkedObjectName></LinkedObjectName>
+            <Rotation>Rotation0</Rotation>
+            <IsMirrored>False</IsMirrored>
+            <IsVariable>False</IsVariable>
+            <Image>
+                iVBORw0KGgoAAAANSUhEUgAAASwAAAEsCAYAAAB5fY51AAAABGdBTUEAALGPC/xhBQAAACBjSFJNAAB6JgAAgIQAAPoAAACA6AAAdTAAAOpgAAA6mAAAF3CculE8AAAABmJLR0QAAAAAAAD5Q7t/AAAACXBIWXMAAABgAAAAYADwa0LPAAAFgklEQVR42u3dMY4TQRBAURtxC8u+/7kc+BpDsGyASHaAZuqP34uR1R4PXxVsqa/btm0XgIBvRx8A4KsEC8gQLCBDsIAMwQIyBAvIECwgQ7CADMECMgQLyBAsIEOwgAzBAjIEC8gQLCBDsIAMwQIyBAvIECwgQ7CADMECMgQLyBAsIEOwgAzBAjIEC8gQLCBDsIAMwQIyBAvIECwgQ7CADMECMr4ffYBPt9vt8nq9jj5GyrZtX/631+v18M+d8By8Z/vteb6rmbCADMECMgQLyBAsIEOwgAzBAjIEC8gQLCBDsIAMwQIyxqzm7PF8Pi/3+/3oYyyxanVk0npF5bzes3lMWECGYAEZggVkCBaQIVhAhmABGYIFZAgWkCFYQIZgARnJ1Zw9are61J5D7YadVSZ8twnv2WomLCBDsIAMwQIyBAvIECwgQ7CADMECMgQLyBAsIEOwgIzTr+Ywx6o1nndYSeGDCQvIECwgQ7CADMECMgQLyBAsIEOwgAzBAjIEC8gQLCDDag6/WbXqYt2Gv2XCAjIEC8gQLCBDsIAMwQIyBAvIECwgQ7CADMECMgQLyDj9ao4Vjw+rbqxZpbbGM+EM78CEBWQIFpAhWECGYAEZggVkCBaQIVhAhmABGYIFZAgWkJFczXk8HkcfIWfVqsuEz13FezaPCQvIECwgQ7CADMECMgQLyBAsIEOwgAzBAjIEC8gQLCDjurnug/9k1bqNV/h9mLCADMECMgQLyBAsIEOwgAzBAjIEC8gQLCBDsIAMwQIyxtyaM+GWlD1WrYOc+RaaVVY9s1VnWGXCbUermbCADMECMgQLyBAsIEOwgAzBAjIEC8gQLCBDsIAMwQIyxtyaU1sVmHDeCWs8Z14PmvC7TTDh/9snExaQIVhAhmABGYIFZAgWkCFYQIZgARmCBWQIFpAhWEDGmFtzJqx47DnDmdc2ar8FHyb8bquZsIAMwQIyBAvIECwgQ7CADMECMgQLyBAsIEOwgAzBAjLGrObUbqFZ5czfbYLac5hw29EkJiwgQ7CADMECMgQLyBAsIEOwgAzBAjIEC8gQLCBDsICM6zbkb/Qn3L6yarVhwnfbY8J5J6yZTDgDvzJhARmCBWQIFpAhWECGYAEZggVkCBaQIVhAhmABGYIFZIxZzWEt6zZzzjBB9TmYsIAMwQIyBAvIECwgQ7CADMECMgQLyBAsIEOwgAzBAjLGrObcbrfL6/U6+hgptfWVCetBE55D7QyTmLCADMECMgQLyBAsIEOwgAzBAjIEC8gQLCBDsIAMwQIyvh99gD/xfD4v9/v96GMsMWFFadWKx5lXR6wd/R8mLCBDsIAMwQIyBAvIECwgQ7CADMECMgQLyBAsIEOwgIzkas4eq1Ym9pi02vAVE9ZMJvxuq6x6Dns+t/ZOfjJhARmCBWQIFpAhWECGYAEZggVkCBaQIVhAhmABGYIFZJx+NYcPE25JOfMZ9nzuhOdQZcICMgQLyBAsIEOwgAzBAjIEC8gQLCBDsIAMwQIyBAvIsJrDX5mw6lJb4+HPmbCADMECMgQLyBAsIEOwgAzBAjIEC8gQLCBDsIAMwQIyTr+aY2WiqbbGs8qe77ZH7Tl8MmEBGYIFZAgWkCFYQIZgARmCBWQIFpAhWECGYAEZggVkJFdzHo/H0Ufgp+qKx7+2aoVmj3f4LUxYQIZgARmCBWQIFpAhWECGYAEZggVkCBaQIVhAhmABGdftHf6eHzgFExaQIVhAhmABGYIFZAgWkCFYQIZgARmCBWQIFpAhWECGYAEZggVkCBaQIVhAhmABGYIFZAgWkCFYQIZgARmCBWQIFpAhWECGYAEZggVkCBaQIVhAhmABGYIFZAgWkCFYQIZgARmCBWQIFpAhWEDGDzAUJFjxRzIvAAAAJXRFWHRkYXRlOmNyZWF0ZQAyMDIzLTAzLTAyVDIxOjUwOjA3KzAwOjAwmRrnnAAAACV0RVh0ZGF0ZTptb2RpZnkAMjAyMy0wMy0wMlQyMTo1MDowNyswMDowMOhHXyAAAAAASUVORK5CYII=</Image>
+            <ScaleMode>Uniform</ScaleMode>
+            <BorderWidth>0</BorderWidth>
+            <BorderColor Alpha="255" Red="0" Green="0" Blue="0" />
+            <HorizontalAlignment>Center</HorizontalAlignment>
+            <VerticalAlignment>Center</VerticalAlignment>
+        </ImageObject>
+        <Bounds X="0" Y="0" Width="1200" Height="1200" />
+    </ObjectInfo>
+</DieCutLabel>''');
+    label.print(getPrinters()[0].name);
+  }
+
   @override
   Widget build(BuildContext context) {
     return StreamBuilder(
@@ -98,12 +127,20 @@ class _ItemPageState extends State<ItemPage> {
                       ),
                     ),
                   ),
-                  Text(
-                      'Browser supported: ${checkEnvironment().isBrowserSupported}'),
-                  Text(
-                      'Framework installed: ${checkEnvironment().isFrameworkInstalled}'),
-                  Text(
-                      'Webservice present: ${checkEnvironment().isWebServicePresent}'),
+                  // Text(
+                  //     'Browser supported: ${checkEnvironment().isBrowserSupported}'),
+                  // Text(
+                  //     'Framework installed: ${checkEnvironment().isFrameworkInstalled}'),
+                  // Text(
+                  //     'Webservice present: ${checkEnvironment().isWebServicePresent}'),
+                  ListView.builder(
+                    shrinkWrap: true,
+                    itemCount: getPrinters().length,
+                    itemBuilder: (context, index) {
+                      return Text(getPrinters()[index].name);
+                    },
+                  ),
+                  ElevatedButton(onPressed: printLabel, child: Text('Print')),
                   SizedBox(height: 20),
                   if (userRole == UserRole.admin)
                     Expanded(child: ItemHistory(itemId: item.id))
--- lib/services/dymo_service.dart
+++ lib/services/dymo_service.dart
@@ -9,10 +9,29 @@ external void init();
 @JS('checkEnvironment')
 external EnvironmentStatus checkEnvironment();
 
+@JS('getPrinters')
+external List<Printer> getPrinters();
+
+@JS('openLabelXml')
+external Label openLabelXml(String xml);
+
 @JS()
 @anonymous
 class EnvironmentStatus {
   bool isBrowserSupported = false;
   bool isFrameworkInstalled = false;
   bool isWebServicePresent = false;
+}
+
+@JS()
+@anonymous
+class Printer {
+  String name = '';
+}
+
+@JS()
+@anonymous
+class Label {
+  @JS('print')
+  external void print(String printerName);
 }
