a533b0440ab5629eb09ce218ef4f2adbdc31d469
adenysenko <a.denysenko@vehikl.com>
2023-03-14 17:10:27 -0400

    Add text to QR code and move it a bit
    Co-authored-by: Alisa Vynohradova <56138100+alisondraV@users.noreply.github.com>

--- lib/services/print_service.dart
+++ lib/services/print_service.dart
@@ -32,7 +32,33 @@ class PrintService {
                   <HorizontalAlignment>Center</HorizontalAlignment>
                   <QuietZonesPadding Left="0" Top="0" Right="0" Bottom="0" />
               </BarcodeObject>
-              <Bounds X="0" Y="0" Width="1600" Height="1600" />
+              <Bounds X="0" Y="0" Width="1200" Height="1600" />
+          </ObjectInfo>
+          <ObjectInfo>
+            <TextObject>
+                <Name>Text</Name>
+                <ForeColor Alpha="255" Red="0" Green="0" Blue="0" />
+                <BackColor Alpha="0" Red="255" Green="255" Blue="255" />
+                <LinkedObjectName></LinkedObjectName>
+                <Rotation>Rotation0</Rotation>
+                <IsMirrored>False</IsMirrored>
+                <IsVariable>True</IsVariable>
+                <HorizontalAlignment>Center</HorizontalAlignment>
+                <VerticalAlignment>Middle</VerticalAlignment>
+                <TextFitMode>ShrinkToFit</TextFitMode>
+                <UseFullFontHeight>True</UseFullFontHeight>
+                <Verticalized>False</Verticalized>
+                <StyledText>
+                    <Element>
+                        <String>Property of Vehikl Inc.</String>
+                        <Attributes>
+                            <Font Family="Arial" Size="6" Bold="False" Italic="False" Underline="False" Strikeout="False" />
+                            <ForeColor Alpha="255" Red="0" Green="0" Blue="0" />
+                        </Attributes>
+                    </Element>
+                </StyledText>
+            </TextObject>
+            <Bounds X="0" Y="1200" Width="1200" Height="400" />
           </ObjectInfo>
       </DieCutLabel>''';
   }
