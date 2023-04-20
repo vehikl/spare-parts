c54675e3bc56c1a9450e744fa2a60bfd92ecf266
adenysenko <a.denysenko@vehikl.com>
2023-04-20 16:47:59 -0400

    Extract and style laptop data

--- lib/pages/item_page/item_page.dart
+++ lib/pages/item_page/item_page.dart
@@ -3,6 +3,7 @@ import 'package:provider/provider.dart';
 import 'package:spare_parts/entities/inventory_item.dart';
 import 'package:spare_parts/entities/inventory_items/laptop.dart';
 import 'package:spare_parts/pages/item_page/item_history.dart';
+import 'package:spare_parts/pages/item_page/laptop_data.dart';
 import 'package:spare_parts/services/repositories/repositories.dart';
 import 'package:spare_parts/utilities/constants.dart';
 import 'package:spare_parts/widgets/error_container.dart';
@@ -47,56 +48,53 @@ class _ItemPageState extends State<ItemPage> {
               child: Column(
                 crossAxisAlignment: CrossAxisAlignment.start,
                 children: [
-                  if (item is Laptop)
-                    Text(item.serialNumber)
-                  else
-                    Card(
-                      shape: RoundedRectangleBorder(
-                        borderRadius: BorderRadius.circular(15.0),
-                      ),
-                      child: Padding(
-                        padding: const EdgeInsets.all(8.0),
-                        child: ListTile(
-                          leading: ItemIcon(item: item),
-                          title:
-                              Text(item.name, style: TextStyle(fontSize: 18)),
-                          subtitle: Column(
-                            crossAxisAlignment: CrossAxisAlignment.start,
-                            children: [
-                              Row(
+                  Card(
+                    shape: RoundedRectangleBorder(
+                      borderRadius: BorderRadius.circular(15.0),
+                    ),
+                    child: Padding(
+                      padding: const EdgeInsets.all(8.0),
+                      child: ListTile(
+                        leading: ItemIcon(item: item),
+                        title: Text(item.name, style: TextStyle(fontSize: 18)),
+                        subtitle: Column(
+                          crossAxisAlignment: CrossAxisAlignment.start,
+                          children: [
+                            Row(
+                              children: [
+                                Icon(Icons.tag_rounded),
+                                Text(item.id),
+                              ],
+                            ),
+                            Row(
+                              children: [
+                                Icon(Icons.domain_rounded),
+                                Text(item.storageLocation != null
+                                    ? item.storageLocation!
+                                    : 'N/A'),
+                              ],
+                            ),
+                            if (item is Laptop) LaptopData(laptop: item),
+                            if (item.description != null)
+                              Column(
+                                crossAxisAlignment: CrossAxisAlignment.start,
                                 children: [
-                                  Icon(Icons.tag_rounded),
-                                  Text(item.id),
+                                  SizedBox(height: 10),
+                                  Text(
+                                    'Description:',
+                                    style:
+                                        TextStyle(fontWeight: FontWeight.bold),
+                                  ),
+                                  Text(item.description!),
                                 ],
                               ),
-                              Row(
-                                children: [
-                                  Icon(Icons.domain_rounded),
-                                  Text(item.storageLocation != null
-                                      ? item.storageLocation!
-                                      : 'N/A'),
-                                ],
-                              ),
-                              if (item.description != null)
-                                Column(
-                                  crossAxisAlignment: CrossAxisAlignment.start,
-                                  children: [
-                                    SizedBox(height: 10),
-                                    Text(
-                                      'Description:',
-                                      style: TextStyle(
-                                          fontWeight: FontWeight.bold),
-                                    ),
-                                    Text(item.description!),
-                                  ],
-                                ),
-                            ],
-                          ),
-                          isThreeLine: true,
-                          trailing: ItemActionsButton(item: item),
+                          ],
                         ),
+                        isThreeLine: true,
+                        trailing: ItemActionsButton(item: item),
                       ),
                     ),
+                  ),
                   SizedBox(height: 20),
                   if (userRole == UserRole.admin)
                     Expanded(child: ItemHistory(itemId: item.id))
---
+++ lib/pages/item_page/laptop_data.dart
@@ -0,0 +1,21 @@
+import 'package:flutter/material.dart';
+import 'package:spare_parts/entities/inventory_items/laptop.dart';
+
+class LaptopData extends StatelessWidget {
+  final Laptop laptop;
+  const LaptopData({super.key, required this.laptop});
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      crossAxisAlignment: CrossAxisAlignment.start,
+      children: [
+        Text(
+          'Serial Number:',
+          style: TextStyle(fontWeight: FontWeight.bold),
+        ),
+        Text(laptop.serialNumber),
+      ],
+    );
+  }
+}
