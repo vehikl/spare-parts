03dce5aadcf0c315a41eea14beb6408c3ba8b5b9
adenysenko <a.denysenko@vehikl.com>
2023-05-03 09:29:22 -0400

    Lint

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -5,39 +5,39 @@ import * as admin from 'firebase-admin'
 admin.initializeApp()
 
 export const deleteIfIncorrectEmail = functions.auth
-  .user()
-  .onCreate((user, context) => {
-    if (!user.email?.endsWith('@vehikl.com')) {
-      console.log(`Deleting user with incorrect email: ${user.email}`)
-      return admin.auth().deleteUser(user.uid)
-    }
-    return null
-  })
+    .user()
+    .onCreate((user, context) => {
+      if (!user.email?.endsWith('@vehikl.com')) {
+        console.log(`Deleting user with incorrect email: ${user.email}`)
+        return admin.auth().deleteUser(user.uid)
+      }
+      return null
+    })
 
 export const associateWithExistingItems = functions.auth
-  .user()
-  .onCreate(async (user, context) => {
-    const db = admin.firestore()
-    const itemsRef = await db
-      .collection('items')
-      .where('borrower.name', '==', user.displayName)
-      .get()
+    .user()
+    .onCreate(async (user, context) => {
+      const db = admin.firestore()
+      const itemsRef = await db
+          .collection('items')
+          .where('borrower.name', '==', user.displayName)
+          .get()
 
-    console.log(`Found ${itemsRef.docs.length} items for ${user.displayName}`);
+      console.log(`Found ${itemsRef.docs.length} items for ${user.displayName}`)
 
-    for (const item of itemsRef.docs) {
-      console.log(`Updating ${item.data().name}`);
-      await item.ref.set({
-        ...item.data(),
-        borrower: {
-          uid: user.uid,
-          name: user.displayName,
-          photoURL: user.photoURL,
-        }
-      })
-    }
-    return null
-  })
+      for (const item of itemsRef.docs) {
+        console.log(`Updating ${item.data().name}`)
+        await item.ref.set({
+          ...item.data(),
+          borrower: {
+            uid: user.uid,
+            name: user.displayName,
+            photoURL: user.photoURL,
+          },
+        })
+      }
+      return null
+    })
 
 
 export const getUsers = functions.https.onCall(async (data, context) => {
